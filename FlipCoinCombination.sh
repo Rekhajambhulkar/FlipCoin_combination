b187fa335ee904237cc1883431b66a653afb1dd3
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 23:53:58 +0530

    Using RANDOM find head or tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,4 +1,14 @@
 #! /bin/bash -x
 
+#print welcome message
 echo "Welcome in FlipCoinCombination"
 
+#Using RANDOM Find head or tail
+choice=$((RANDOM%2))
+
+if [ $choice -eq 1 ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
