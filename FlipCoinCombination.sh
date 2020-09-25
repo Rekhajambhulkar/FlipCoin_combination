da6c4d845da6826058417dee538a8ca220f58936
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-26 01:20:21 +0530

    Percentage of singlet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,19 @@
 #! /bin/bash -x
 
+declare -A Dict
+
 #print welcome message
 echo "Welcome in FlipCoinCombination"
 
+read -p "Enter the option" Singlet
+
 #Using RANDOM Find head or tail
-choice=$((RANDOM%2))
+
+function Flip()
+{
+	choice=$((RANDOM%2))
+	echo $choice
+}
 
 if [ $choice -eq 1 ]
 then
@@ -12,3 +21,13 @@ then
 else
 	echo "Tail"
 fi
+
+#use function to calculate percentage
+function Percentage()
+{
+	range=$1
+	for keyCount in ${Dict[@]}
+	do
+		Dict[keyCount]=`echo "scale=4; $(Dict[$ketCount])/$range*100"}`
+	done
+}
