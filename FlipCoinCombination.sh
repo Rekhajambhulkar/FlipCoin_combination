41f8bf2930c6225c366baf89cdb61444ca9ac5b6
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-26 11:19:37 +0530

    Store Singlet in Dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -5,29 +5,36 @@ declare -A Dict
 #print welcome message
 echo "Welcome in FlipCoinCombination"
 
-read -p "Enter the option" Singlet
+read -p "Enter the option
+	1.Singlet " select
 
 #Using RANDOM Find head or tail
-
-function Flip()
+Flip()
 {
 	choice=$((RANDOM%2))
 	echo $choice
 }
 
+Flip
+
 if [ $choice -eq 1 ]
 then
-	echo "Head"
-else
 	echo "Tail"
+else
+	echo "Head"
 fi
 
 #use function to calculate percentage
-function Percentage()
+Percentage()
 {
 	range=$1
 	for keyCount in ${Dict[@]}
 	do
-		Dict[keyCount]=`echo "scale=4; $(Dict[$ketCount])/$range*100"}`
+		Dict[keyCount]=`echo "scale=4; $(Dict[$ketCount])/$range*100"`;
+		echo "Percentage of $keyCount is: ${Dict[$keyCount]}"
 	done
 }
+#call the function
+Percentage
+
+
